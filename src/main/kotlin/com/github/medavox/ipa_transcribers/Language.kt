ea6a85d21f9a9ab3a0971e24968e1b4726860f2f
Adam Howard <achdisposable1+github@gmail.com>
2018-11-23 11:08:22 +0000

    add missing languages
    accidentally copied the list of languages I intend to support
    from the wrong source.

--- src/main/kotlin/com/github/medavox/ipa_transcribers/Language.kt
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/Language.kt
@@ -1,19 +1,27 @@
 package com.github.medavox.ipa_transcribers
 
 enum class Language(val code: String) {
+    Spanish ("es"),
     English ("en"),
     Arabic ("ar"), //I intend this to mean Classical Arabic which, thanks to the Qu'ran,
     // should be understood by most arabic dialect speakers
     Hindi ("hi"),
     Bengali ("bn"),
     Portuguese ("pt"),
+    Russian ("ru"),
+    Japanese ("ja"),
     Punjabi ("pa"), //listed on Ethnologue as Lahnda with related dialects,
     // but I've excluded them here.
     Javanese ("jw"),
     Turkish ("tr"),
+    Korean ("ko"),
+    French ("fr"),
+    German ("de"),
     Telugu ("te"),
     Marathi ("mr"),
     Urdu ("ur"),
+    Vietnamese ("vi"),
     Tamil ("ta"),
+    Italian ("it"),
     Persian ("fa"),
 }

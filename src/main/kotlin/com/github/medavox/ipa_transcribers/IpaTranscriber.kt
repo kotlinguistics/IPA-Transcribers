db69a5a61495c167a84d0ae7734f0dc812a88a09
Adam Howard <achdisposable1+github@gmail.com>
2018-12-03 23:05:41 +0000

    kdoc pass

--- src/main/kotlin/com/github/medavox/ipa_transcribers/IpaTranscriber.kt
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/IpaTranscriber.kt
@@ -1,5 +1,17 @@
 package com.github.medavox.ipa_transcribers
 
+/**Converts text in the native orthography of a language,
+ * into a collection of broad IPA transcriptions for important language variants.
+ *
+ *  The aim here is not to capture the nuances of every dialect/accent of a language;
+ *  but rather to provide the pronunciation of a standard or prestige dialect(s)
+ *  that every speaker should be able to understand.
+ *
+ *  For instance, English IPA transcribers should provide American English and British English
+ *  transcriptions, but ignore Irish, Scottish, Texan, Indian, African, and International variants.
+ *
+ *  To get an instance for a particular language,
+ *  call [TranscriberFactory.getTranscriberForLang]*/
 interface IpaTranscriber {
     fun transcribeToIpa(nativeText:String):Set<Variant>
 }
--- src/main/kotlin/com/github/medavox/ipa_transcribers/TranscriberFactory.kt
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/TranscriberFactory.kt
@@ -1,8 +1,12 @@
 package com.github.medavox.ipa_transcribers
 
-
-
+/**Provides instances that each transcribe a specific language and orthography to IPA.*/
 object TranscriberFactory {
+
+    /**Get an [IpaTranscriber] instance for the specified language.
+     * @param language the language to get an [IpaTranscriber] instance for.
+     * If there is no [IpaTranscriber] implementation available for the specified language,
+     * A dummy implementation is returned which only returns empty sets.*/
     fun getTranscriberForLang(language: Language): IpaTranscriber {
         return when(language) {
             Language.English -> EnglishIpaCambridge()

93e3f59b08f36f00632f2554313355c5d990c16b
Adam Howard <achdisposable1+github@gmail.com>
2019-01-01 12:05:52 +0000

    fill out IPA consonants more
    Former-commit-id: 4f4873cca777036791cc54535300a48b90f569e2

--- src/main/kotlin/com/github/medavox/ipa/Consonants.kt
+++ src/main/kotlin/com/github/medavox/ipa/Consonants.kt
@@ -1,13 +1,50 @@
 package com.github.medavox.ipa
 
+import com.github.medavox.ipa.Manner.*
+import com.github.medavox.ipa.Place.*
+
 enum class Manner {
     STOP_PLOSIVE,
     NASAL,
-    TRILL,
-    TAP_OR_FLAP,
     FRICATIVE,
-    LATERAL_FRICATIVE,
     APPROXIMANT,
-    LATERAL_APPROXIMANT
+    TAP_OR_FLAP,
+    TRILL,
+    LATERAL_FRICATIVE,
+    LATERAL_APPROXIMANT,
+    LATERAL_TAP_FLAP,
+
+
+}
+
+enum class Place {
+    //labial
+    BILABIAL,
+    LABIO_DENTAL,
+    LINGUO_LABIAL,
+
+    //coronal
+    DENTAL,
+    ALVEOLAR,
+    POST_ALVEOLAR,
+    RETROFLEX,
+
+    //dorsal
+    PALATAL,
+    VELAR,
+    UVULAR,
+
+    //laryngeal
+    PHARYNGEAL_EPIGLOTTAL,
+    GLOTTAL
+}
+
+enum class PulmonicConsonants(ipaSymbol:String,
+                              placeOfArticulation:Place,
+                              mannerOfArticulation:Manner,
+                              voiced:Boolean) {
+    ubs("p", BILABIAL, STOP_PLOSIVE, false),
+    vbs("b", BILABIAL, STOP_PLOSIVE, true),
+
 
 }

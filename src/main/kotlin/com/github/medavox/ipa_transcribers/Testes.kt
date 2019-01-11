70141bd7d7c2d8c8459aba28856158b23dfcd5ee
Adam Howard <achdisposable1+github@gmail.com>
2019-01-11 11:51:37 +0000

    remove main class
    this file is only for testing the library during development,
    and doesn't belong in the library proper.
    
    It's also a very small file whose contents are frequently replaced,
    so no information would be lost by not storing it in Git.
    
    
    Former-commit-id: d68359ba827d8ba0f05fbc1d8686a6c986957664

--- src/main/kotlin/com/github/medavox/ipa_transcribers/Testes.kt
+++
@@ -1,8 +0,0 @@
-package com.github.medavox.ipa_transcribers
-
-fun main(args:Array<String>) {
-    //System.out.println(EnglishIpaCambridge().transcribeToIpa("cacophony"))
-    //System.out.println(SpanishIpaRuleBased().transcribeToIpa("Cinco de Mayo"))
-    System.out.println(SpanishIpaRuleBased().transcribeToIpa("halla haya"))
-    //System.out.println("result:${-50 % 2}")
-}

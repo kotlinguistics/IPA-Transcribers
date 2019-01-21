731eafa3510eec0e0dc34a0bb66d84531d536e6d
Adam Howard <achdisposable1+github@gmail.com>
2019-01-21 10:43:49 +0000

    finish creating new language variant API
    Former-commit-id: 7025645c59a8b6d1d9c52e12cacf9b9bb8ad41b9

--- src/main/kotlin/com/github/medavox/ipa_transcribers/Language.kt
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/Language.kt
@@ -1,27 +1,50 @@
 package com.github.medavox.ipa_transcribers
 
-enum class Language(val code: String) {
-    Spanish ("es"),
-    English ("en"),
-    Arabic ("ar"), //I intend this to mean Classical Arabic which, thanks to the Qu'ran,
+import com.github.medavox.ipa_transcribers.Language.*
+
+sealed class Language(val code: String) {
+    class Spanish : Language("es")
+    class English : Language("en")
+    class Arabic : Language("ar") //I intend this to mean Classical Arabic which, thanks to the Qu'ran,
     // should be understood by most arabic dialect speakers
-    Hindi ("hi"),
-    Bengali ("bn"),
-    Portuguese ("pt"),
-    Russian ("ru"),
-    Japanese ("ja"),
-    Punjabi ("pa"), //listed on Ethnologue as Lahnda with related dialects,
+    class Hindi : Language("hi")
+    class Bengali : Language("bn")
+    class Portuguese : Language("pt")
+    class Russian : Language("ru")
+    class Japanese : Language("ja")
+    class Punjabi : Language("pa") //listed on Ethnologue as Lahnda with related dialects,
     // but I've excluded them here.
-    Javanese ("jw"),
-    Turkish ("tr"),
-    Korean ("ko"),
-    French ("fr"),
-    German ("de"),
-    Telugu ("te"),
-    Marathi ("mr"),
-    Urdu ("ur"),
-    Vietnamese ("vi"),
-    Tamil ("ta"),
-    Italian ("it"),
-    Persian ("fa"),
+    class Javanese : Language("jw")
+    class Turkish : Language("tr")
+    class Korean : Language("ko")
+    class French : Language("fr")
+    class German : Language("de")
+    class Telugu : Language("te")
+    class Marathi : Language("mr")
+    class Urdu : Language("ur")
+    class Vietnamese : Language("vi")
+    class Tamil : Language("ta")
+    class Italian : Language("it")
+    class Persian : Language("fa")
+}
+
+sealed class Variant<Language> {
+    object All : Variant<Language>()
+
+    object British : Variant<English>()
+    object American : Variant<English>()
+
+    object Peninsular : Variant<Spanish>()
+    object PanAmerican : Variant<Spanish>()
+}
+
+sealed class GenericRule(matcher:Regex, lettersConsumed: Int?)
+
+class  VariantRule<T:Language>(val matcher:Regex,
+                               vararg val variantOutputs:Map.Entry<Variant<T>, String>,
+                               lettersConsumed: Int?=null):GenericRule(matcher, lettersConsumed)
+
+data class Rool(val matcher:Regex, val outputString:() -> String, val lettersConsumed:Int?=null ):GenericRule(matcher, lettersConsumed) {
+    constructor(matcher:Regex, outputString: String, lettersConsumed:Int?=null)
+            :this(matcher, {outputString}, lettersConsumed)
 }
--- src/main/kotlin/com/github/medavox/ipa_transcribers/Variant.kt
+++
@@ -1,3 +0,0 @@
-package com.github.medavox.ipa_transcribers
-
-data class Variant(val variantName:String, val transcription:String)

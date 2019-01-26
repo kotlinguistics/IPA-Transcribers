9305bf6e7a8c371eb54983cdcbcf594f3b736cb6
Adam Howard <achdisposable1+github@gmail.com>
2019-01-26 01:56:37 +0000

    remove Oxford English->IPA Transcriber
    this transcriber has stopped working, probably due to a site redesign.
    I don't want to break off and repair a scraper while I'm focused on API design.
    
    
    Former-commit-id: c70095a9eb02dd75c2f51b3ea7ac939511d55d17

--- src/main/kotlin/com/github/medavox/ipa_transcribers/TranscriberFactory.kt
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/TranscriberFactory.kt
@@ -1,6 +1,6 @@
 package com.github.medavox.ipa_transcribers
 
-import com.github.medavox.ipa_transcribers.english.EnglishIpaCambridge
+//import com.github.medavox.ipa_transcribers.english.EnglishIpaCambridge
 import com.github.medavox.ipa_transcribers.spanish.SpanishIpaRuleBased
 import com.github.medavox.ipa_transcribers.Language.*
 
@@ -14,7 +14,7 @@ object TranscriberFactory {
     fun getTranscriberForLang(language: Class<Language>): Transcriber<*> {
       //todo:instead of directly transcribing arabic to IPA, try transcribing the google-romanised text to IPA
         return when(language) {
-            is English -> EnglishIpaCambridge()
+            //is English -> EnglishIpaCambridge()
             is Spanish -> SpanishIpaRuleBased()
             else -> UselessDefaultIpaTranscriber()
         }

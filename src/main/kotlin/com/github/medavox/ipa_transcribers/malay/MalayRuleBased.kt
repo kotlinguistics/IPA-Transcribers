4cdbe4f6357b840c351f3f1866886a27f6369770
Adam Howard <achdisposable1+github@gmail.com>
2019-02-08 14:39:16 +0000

    add malay rule-based transcriber
    Former-commit-id: 89dcedbee57ad05186e532d507394eeeaeb4547a

--- src/main/kotlin/com/github/medavox/ipa_transcribers/malay/MalayRuleBased.kt
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/malay/MalayRuleBased.kt
@@ -1,2 +1,25 @@
 package com.github.medavox.ipa_transcribers.malay
 
+import com.github.medavox.ipa_transcribers.Language.IndonesianMalay
+import com.github.medavox.ipa_transcribers.Transcriber
+import com.github.medavox.ipa_transcribers.baserules.BaseScriptRules
+import com.github.medavox.ipa_transcribers.rulesystem.Rule
+import com.github.medavox.ipa_transcribers.rulesystem.RuleProcessor
+
+object MalayRuleBased:Transcriber<IndonesianMalay>, RuleProcessor<IndonesianMalay> {
+    val rules:List<Rule> = listOf(
+        Rule("c", "t͡ʃ"),
+        Rule("j", "d͡ʒ"),
+        Rule("ai", "ai̯"),
+        Rule("au", "au̯"),
+        Rule("oi", "oi̯̯"),//or ʊi
+        Rule("gh", "ɣ"),// or x
+        Rule("kh", "x"),
+        Rule("ng", "ŋ"),
+        Rule("ny", "ɲ"),
+        Rule("sy", "ʃ")
+    ) + BaseScriptRules.latinBaseRules
+    override fun transcribe(nativeText: String): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}

3a4c553fdb4fd7bf483255b80c40b5b13ba36d76
Adam Howard <achdisposable1+github@gmail.com>
2019-02-08 10:45:04 +0000

    add sets of common pronunciation rules for shared scripts
    Former-commit-id: 5cb13b24be0dd9334b32a52c224cb928aa8a1afe

---
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/baserules/BaseScriptRules.kt
@@ -0,0 +1,42 @@
+package com.github.medavox.ipa_transcribers.baserules
+
+import com.github.medavox.ipa_transcribers.rulesystem.Rule
+
+/**common pronunciation rules for scripts shared by multiple languages*/
+object BaseScriptRules {
+    val devanagariBaseRules:List<Rule> = listOf(
+
+    )
+
+    val latinBaseRules:List<Rule> = listOf(
+        Rule("a", "a"),
+        Rule("b", "b"),
+        Rule("d", "d"),
+
+        Rule("e", "e"),//[a]
+        Rule("f", "f"),
+
+        Rule("g", "ɡ"), ///ɟ/[b]
+
+        Rule("h", "h"),
+        Rule("i", "i"),
+        Rule("k", "k"),//, /c/[b]
+        Rule("l", "l"),//, /l/[b]
+        Rule("m", "m"),
+        Rule("n", "n"),
+        Rule("o", "o"),
+        Rule("p", "p"),
+
+        Rule("s", "s"),
+        Rule("t", "t"),
+        Rule("u", "u"),
+        Rule("v", "v"),
+        Rule("w", "w"),
+        Rule("y", "j"),
+        Rule("z", "z")
+    )
+
+    val persoArabicBaseRules:List<Rule> = listOf(
+
+    )
+}

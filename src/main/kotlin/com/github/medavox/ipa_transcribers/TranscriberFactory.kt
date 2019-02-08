607dfc08f3ea247f894ec6785d72204822cd2253
Adam Howard <achdisposable1+github@gmail.com>
2019-02-08 23:05:20 +0000

    add finished transcribers to factory
    Former-commit-id: 3ebd313c394bcdc9f1a0f0eb3009653893147b56

--- src/main/kotlin/com/github/medavox/ipa_transcribers/TranscriberFactory.kt
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/TranscriberFactory.kt
@@ -3,7 +3,14 @@ package com.github.medavox.ipa_transcribers
 //import com.github.medavox.ipa_transcribers.english.EnglishIpaCambridge
 import com.github.medavox.ipa_transcribers.spanish.PanAmericanSpanishIpaRuleBased
 import com.github.medavox.ipa_transcribers.Language.*
+import com.github.medavox.ipa_transcribers.french.FrenchJsoupEasyPronunciation
+import com.github.medavox.ipa_transcribers.japanese.JapaneseJsoupEasyPronunciation
+import com.github.medavox.ipa_transcribers.korean.KoreanRuleBased
+import com.github.medavox.ipa_transcribers.malay.MalayRuleBased
+import com.github.medavox.ipa_transcribers.portuguese.EuropeanPortugueseJsoupEasyPronunciation
+import com.github.medavox.ipa_transcribers.russian.RussianJsoupEasyPronunciation
 import com.github.medavox.ipa_transcribers.spanish.PeninsularSpanishRuleBased
+import com.github.medavox.ipa_transcribers.turkish.TurkishRuleBased
 
 /**Provides instances that each transcribe a specific language and orthography to IPA.*/
 object TranscriberFactory {
@@ -16,8 +23,15 @@ object TranscriberFactory {
       //todo:instead of directly transcribing arabic to IPA, try transcribing the google-romanised text to IPA
         return when(language) {
             //is English -> EnglishIpaCambridge()
-            is PanAmericanSpanish -> PanAmericanSpanishIpaRuleBased
-            is PeninsularSpanish -> PeninsularSpanishRuleBased
+            PanAmericanSpanish -> PanAmericanSpanishIpaRuleBased
+            PeninsularSpanish -> PeninsularSpanishRuleBased
+            Turkish -> TurkishRuleBased
+            Korean -> KoreanRuleBased
+            IndonesianMalay -> MalayRuleBased
+            Japanese -> JapaneseJsoupEasyPronunciation
+            Russian -> RussianJsoupEasyPronunciation
+            French -> FrenchJsoupEasyPronunciation
+            EuropeanPortuguese -> EuropeanPortugueseJsoupEasyPronunciation
             else -> UselessDefaultIpaTranscriber()
         }
     }

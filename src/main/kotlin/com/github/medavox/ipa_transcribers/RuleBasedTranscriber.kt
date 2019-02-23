aedfaf5985fbdf6c748c00b5bf193c41c088a8ba
Adam Howard <achdisposable1+github@gmail.com>
2019-02-23 10:31:28 +0000

    add failed experimental rule processing function 'greedy'
    uses the match with the highest consumed-characters each time
    
    
    Former-commit-id: 06779619bbf8d64dc47c1f4cd885fdb73e1e96cb

--- src/main/kotlin/com/github/medavox/ipa_transcribers/RuleBasedTranscriber.kt
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/RuleBasedTranscriber.kt
@@ -166,6 +166,33 @@ interface RuleBasedTranscriber<T:Language>:Transcriber<T> {
         Rule(" ", " ")
     )
 
+    /*Automatically applies the rule which consumes the most characters*/
+    /*fun String.greedy(rules: List<Rule>, onNoRuleMatch: (unmatched: String) -> UnmatchedOutput):String {
+        var out:String = ""
+        var processingWord = this
+        loop@ while(processingWord.isNotEmpty()) {
+            //val matchResult:MatchResult? = rule.matcher.find(processingWord)
+            val rule:Rule? = rules.filter { it.matcher.find(processingWord)?.range?.start == 0 }
+                .sortedBy { it.lettersConsumed }.lastOrNull()
+            if(rule == null) {
+                //no rule matched; call the lambda!
+                val unmatchedOutput = onNoRuleMatch(processingWord)
+                processingWord = unmatchedOutput.newWorkingInput
+                out = unmatchedOutput.output(out)
+            }else {
+                out = rule.outputString(out)
+                System.out.println("matched rule:$rule")
+                //number of letters consumed is the match length, unless explicitly specified
+                val actualLettersConsumed = rule.lettersConsumed ?: rule.matcher.find(processingWord)?.value?.length ?: 0
+                if(actualLettersConsumed > 0) {
+                    processingWord = processingWord.substring(actualLettersConsumed)
+                    continue@loop
+                }//else keep going through the rule list
+            }
+        }
+        return out
+    }*/
+
     fun String.processWithRules(rules:List<Rule>,
                                 onNoRuleMatch:(unmatched:String) -> UnmatchedOutput
     ) : String {

f2146b7c2cef4b7c6283a6ee09ef10615492496f
Adam Howard <achdisposable1+github@gmail.com>
2019-02-23 10:33:47 +0000

    discuss limitations of rule processor for russian
    Former-commit-id: 55286cdf68c08931c94ecab8b983668e93a2c5e5

--- src/main/kotlin/com/github/medavox/ipa_transcribers/russian/RussianRuleBased.kt
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/russian/RussianRuleBased.kt
@@ -5,6 +5,41 @@ import com.github.medavox.ipa_transcribers.Rule
 import com.github.medavox.ipa_transcribers.RuleBasedTranscriber
 
 object RussianRuleBased:RuleBasedTranscriber<Russian> {
+    //features of russian orthography
+    //1.hardening and softening of consonants
+    //vowel reduction
+    //the third one
+    private const val softenableConsonants = "бвгдзклмнпрстфх"
+    //problem:
+
+    //we need to know the previous consonant to check if it can be softened;
+    //but we also need to print out the following vowel so we don't reprocess it again as a word-initial.
+
+    //this means one rule for every pair of softenable consonant + softening vowel,
+    //or 5 * 15 = 75 rules.
+
+    //this is bad.
+
+    //so instead we need to be able to 'look back' at previous, consumed letters -- preferably in a Regex-friendly way.
+    //so maybe instead of feeding the chopped-off string (with the characters consumed so far removed),
+    //we feed in the whole string each time, with a pointer to the current one?
+
+    //maybe we have to match regexes on two strings: the consumed-so-far characters AND the unconsumed characters?
+
+    //like this:
+
+    // para aq | ui
+
+    //this would allow us to use regex to read the end of the last-consumed characters
+
+    //Consonant letters represent both
+    // "soft" (palatalized, represented in the IPA with a ⟨ʲ⟩) and
+    // "hard" consonant phonemes.
+    //
+    // If consonant letters are followed by vowel letters,
+    // the soft/hard quality of the consonant depends on whether the vowel is meant to follow
+    // "hard" consonants ⟨а, о, э, у, ы⟩ or
+    // "soft" consonants ⟨я, ё, е, ю, и⟩
     val rules:List<Rule> = listOf(
         Rule("а", "a"),
         Rule("б", "b"), //  or /bʲ/

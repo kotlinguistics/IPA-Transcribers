090d13b13e2f4cc7418b8ae3d91df5dd81e15e4b
Adam Howard <achdisposable1+github@gmail.com>
2019-03-03 12:50:41 +0000

    add notes to persian rule-based transcriber
    Former-commit-id: 8d3b0f23b7c0944d95e0e99cdb0c5cf86f3ee116

--- src/main/kotlin/com/github/medavox/ipa_transcribers/persian/PersianRuleBased.kt
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/persian/PersianRuleBased.kt
@@ -3,6 +3,21 @@ package com.github.medavox.ipa_transcribers.persian
 import com.github.medavox.ipa_transcribers.Language.Persian
 import com.github.medavox.ipa_transcribers.RuleBasedTranscriber
 
+/**Persian, like Modern Standard Arabic, does not write short vowels.
+ * This means that any transcription system that relies solely on the orthography (such as this one)
+ * *cannot* produce a complete transcription.
+ *
+ * A lookup dictionary wouldn't even solve this problem. According to Wikipedia:
+ *
+ * "words distinguished from each other only by short vowels are ambiguous in writing:
+ * Western Persian kerm "worm", karam "generosity", kerem "cream",
+ * and krom "chrome" are all spelled krm (کرم) in Persian"
+ *
+ * "Context" is necessary for first working out which word the vowel-less spelling refers to;
+ * and context is no simple thing, requiring tools more complex than regex matching.
+ *
+ * Even so, this transcriber does the best job possible,
+ * given the information that *has* been recorded in the orthography.*/
 object PersianRuleBased:RuleBasedTranscriber<Persian> {
     override fun transcribe(nativeText: String): String {
         return nativeText.processWithRules(persoArabicBaseRules, reportAndCopy)

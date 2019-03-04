35425fd84bbb2639a868e1cf8ba3696847aca93f
Adam Howard <achdisposable1+github@gmail.com>
2019-03-04 17:50:14 +0000

    add Google Arabic Romanisation -> IPA
    this was an experiment to see if their transliteration
    was more accurate than our purely rule-based one could be;
    
    They do indeed record slightly more vowel information,
    but they heavily simplify the consonants,
    ignoring any unpronouncable features for monolingual English speakers.
    
    
    Former-commit-id: b5bd8242fb491c1cd97971052c03493e20f89521

---
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/arabic/GoogleRomanisedArabic.kt
@@ -0,0 +1,30 @@
+package com.github.medavox.ipa_transcribers.arabic
+
+import com.github.medavox.ipa_transcribers.Language
+import com.github.medavox.ipa_transcribers.Rule
+import com.github.medavox.ipa_transcribers.RuleBasedTranscriber
+//write google -> IPA arabic transcriber
+/**Google Translate's romanisation of Arabic includes vowel information not present in the Arabic text.
+ * Maybe they have a lookup dictionary.
+ * So this transcriber takes their romanisation and converts it to IPA.
+ *
+ * However, Google's romanisation also throws away a lot of the original information in the arabic text.
+ * For example, glottalised consonants are not transliterated differently than glottalised consonants,
+ * despite these being distinguished in Arabic.*/
+object GoogleRomanisedArabic: RuleBasedTranscriber<Language> {
+    val rules:List<Rule> = listOf<Rule>(
+        Rule("'", "ʕ"),
+        Rule("gh", "ɣ"),
+        Rule("sh", "ʃ"),
+        Rule("kh", "x"),
+        Rule("th", "θ"),
+        Rule("dh", "ð"),
+        Rule("j", "ʒ"),
+        Rule("h", "ħ"),
+        Rule("q", "q")//the same sound is repsented with a Q in arabic
+
+    )+ latinBaseRules + westernPunctuation
+    override fun transcribe(nativeText: String): String {
+        return nativeText.processWithRules(rules, reportAndCopy)
+    }
+}

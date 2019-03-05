f00a5c23c84ad3b379a9da0cf8f283e8384547fc
Adam Howard <achdisposable1+github@gmail.com>
2019-03-05 08:56:39 +0000

    begin japanese rule-based hiragana & katakana
    Former-commit-id: d0e464d1a70b4c7fdec4986a6f1e5b2d78643290

--- src/main/kotlin/com/github/medavox/ipa_transcribers/asian_unique/JapaneseRuleBasedKatakanaHiraganaOnly.kt
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/asian_unique/JapaneseRuleBasedKatakanaHiraganaOnly.kt
@@ -1,2 +1,15 @@
 package com.github.medavox.ipa_transcribers.asian_unique
 
+import com.github.medavox.ipa_transcribers.Rule
+import com.github.medavox.ipa_transcribers.RuleBasedTranscriber
+
+/**We can't process Japanese's primary writing system, Kanji, with a rule-based transcriber.
+ * But we can process its simpler supplementary writing systems, Katakana and Hiragana.*/
+object JapaneseRuleBasedKatakanaHiraganaOnly:RuleBasedTranscriber {
+    private val rules:List<Rule> = listOf(
+
+    )
+    override fun transcribe(nativeText: String): String {
+        return nativeText.processWithRules(rules, reportAndCopy)
+    }
+}

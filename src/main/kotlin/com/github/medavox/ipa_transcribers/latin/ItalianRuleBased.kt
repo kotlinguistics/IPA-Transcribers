a147d9ce0fc8a41d332ea4b5612765e1259fefae
Adam Howard <achdisposable1+github@gmail.com>
2019-07-16 10:11:45 +0100

    Italian: working on sibilants
    Former-commit-id: b1880748808ca7ff80031da26ca881b2306e34bf

--- src/main/kotlin/com/github/medavox/ipa_transcribers/latin/ItalianRuleBased.kt
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/latin/ItalianRuleBased.kt
@@ -11,6 +11,12 @@ import com.github.medavox.ipa_transcribers.RuleBasedTranscriber
  * According to Wikipedia, Northern Italy is more densely populated than Southern Italy;
  * therefore this transcriber will favour Northern pronunciations.*/
 object ItalianRuleBased: RuleBasedTranscriber() {
+    val alphabet = "abcdefghilmnopqrstuvz"
+    val vowels = "aeiou"
+    val voicedConsonants = "bdglmnrvz"
+    val unvoicedConsonants= "cfhpqst"
+    val consonants = voicedConsonants+unvoicedConsonants
+
     val frontVowels = "ie"
     val nonFrontVowels = "aou"
     override val completionStatus: CompletionStatus = CompletionStatus.IN_PROGRESS
@@ -59,41 +65,53 @@ object ItalianRuleBased: RuleBasedTranscriber() {
         //S and Z
         //⟨s⟩ and ⟨z⟩ are ambiguous to voicing.
 
-        //⟨s⟩ represents a dental sibilant consonant, either /s/ or /z/. However, these two phonemes are in complementary distribution everywhere except between two vowels in the same word and, even with such words, there are very few minimal pairs.
+        //⟨s⟩ represents a dental sibilant consonant, either /s/ or /z/.
+        // However, these two phonemes are in complementary distribution everywhere except
+        // between two vowels in the same word and, even with such words, there are very few minimal pairs.
         //
-        //    The voiceless /s/ occurs:
-        //        At the start of a word before a vowel (e.g. Sara /ˈsara/) or a voiceless consonant (e.g. spuntare /spunˈtare/)
-        //        After any consonant (e.g. transitare /transiˈtare/)
-        //        Before a voiceless consonant (e.g. raspa /ˈraspa/)
-        //        At the start of the second part of a compound word (e.g. affittasi, disotto, girasole, prosegue, risaputo, unisono, preservare, riservare, reggiseno). These words are formed by adding a prefix to a word beginning with /s/
-        //    The voiced /z/ occurs before voiced consonants (e.g. sbranare /zbraˈnare/).
-        //    It can be either voiceless or voiced (/s/ or /z/) between vowels;
-        //    in standard Tuscany-based pronunciation some words are pronounced with /s/ between vowels
-        //    (e.g. casa, cosa, così, mese, naso, peso, cinese, piemontese, goloso);
-        //    in Northern Italy (and also increasingly in Tuscany) ⟨s⟩ between vowels is always pronounced with /z/
-        //    (with some exceptions[example needed]) whereas in Southern Italy ⟨s⟩ between vowels is always pronounced /s/.
+        //The voiceless /s/ occurs:
+        //    At the start of a word before a vowel (e.g. Sara /ˈsara/) or a voiceless consonant (e.g. spuntare /spunˈtare/)
+        Rule("(| )", "s([$vowels]|$unvoicedConsonants)", "s", 1),
+        //    After any consonant (e.g. transitare /transiˈtare/)
+        Rule("[$consonants]", "s", "s"),
+        //    Before a voiceless consonant (e.g. raspa /ˈraspa/)
+        Rule("s[$unvoicedConsonants]", "s", 1),
+        //todo:At the start of the second part of a compound word (e.g.
+        //      affittasi, disotto, girasole, prosegue, risaputo, unisono, preservare, riservare, reggiseno).
+        //      These words are formed by adding a prefix to a word beginning with /s/
+
+        //The voiced /z/ occurs before voiced consonants (e.g. sbranare /zbraˈnare/).
+        Rule("s[$voicedConsonants]", "z", 1),
+        //It can be either voiceless or voiced (/s/ or /z/) between vowels;
+        //in standard Tuscany-based pronunciation some words are pronounced with /s/ between vowels
+        //(e.g. casa, cosa, così, mese, naso, peso, cinese, piemontese, goloso);
+        //in Northern Italy (and also increasingly in Tuscany) ⟨s⟩ between vowels is always pronounced with /z/
+        Rule("[$vowels]", "s[$vowels]", "z", 1),
+        //(with some exceptions[example needed]) whereas in Southern Italy ⟨s⟩ between vowels is always pronounced /s/.
 
         //⟨ss⟩ always represents voiceless /ss/: grosso /ˈɡrɔsso/, successo /sutˈtʃɛsso/, passato /pasˈsato/, etc.
         Rule(Regex("ss"), "ss"),
-
-        //alternative spelling of /f/, probably from greek loanwords, like in english
-        Rule("ph", "f"),
-        //h is always silent, or forms part of a digraph
-        Rule("h", "")
 
         //⟨z⟩ represents a dental affricate consonant; either /dz/ (zanzara /dzanˈdzara/) or /ts/ (nazione /natˈtsjone/),
         // depending on context, though there are few minimal pairs.
 
         //It is normally voiceless /ts/:
         //    At the start of a word in which the second syllable starts with a voiceless consonant
+        Rule("(| )", "z[$vowels]+"),//fixme: italian syllabification rules. M is evidently a part of the first syllable
         //        (zampa /ˈtsampa/, zoccolo /ˈtsɔkkolo/, zufolo /ˈtsufolo/)
         //        Exceptions (because they are of Greek origin): zaffiro, zefiro, zotico, zeta, zafferano, Zacinto
-        //    When followed by an ⟨i⟩ which is followed, in turn, by another vowel (e.g. zio /ˈtsio/, agenzia /adʒenˈtsia/, grazie /ˈɡrattsje/)
-        //        Exceptions: azienda /adˈdzjɛnda/, all words derived from words obeying other rules (e.g. romanziere /romanˈdzjɛre/, which is derived from romanzo)
+        //    When followed by an ⟨i⟩ which is followed, in turn, by another vowel (e.g.
+        //        zio /ˈtsio/, agenzia /adʒenˈtsia/, grazie /ˈɡrattsje/)
+        Rule("zi[$vowels]", "ts", 1),
+        //        Exceptions: azienda /adˈdzjɛnda/, all words derived from words obeying other rules (e.g.
+        //        romanziere /romanˈdzjɛre/, which is derived from romanzo)
         //    After the letter ⟨l⟩ (e.g. alzare /alˈtsare/)
+        Rule("l", "z", "ts"),
         //        Exceptions: elzeviro /eldzeˈviro/ and Belzebù /beldzeˈbu/
         //    In the suffixes -anza, -enza and -onzolo (e.g. usanza /uˈzantsa/, credenza /kreˈdɛntsa/, ballonzolo /balˈlontsolo/)
-
+        Rule("anza", "antsa"),
+        Rule("enza", "ɛntsa"),
+        Rule("onzolo", "ontsolo"),
         //It is voiced /dz/:
         //    At the start of a word in which the second syllable starts with a voiced consonant or ⟨z⟩ (or ⟨zz⟩) itself
         //        (e.g. zebra /ˈdzɛbra/, zuzzurellone /dzuddzurelˈlone/)
@@ -108,6 +126,13 @@ object ItalianRuleBased: RuleBasedTranscriber() {
         // A major exception is the verbal ending -izzare (from Greek -ίζειν),
         // in which it is always pronounced /ddz/ (e.g. organizzare /ɔrɡanidˈdzare/),
         // and derived words (e.g. analizzo /anaˈliddzo/, a derivative of analizzare).
+
+
+        //alternative spelling of /f/, probably from greek loanwords, like in english
+        Rule("ph", "f"),
+        //h is always silent, or is part of a digraph
+        Rule("h", "")
+
 
         //In addition to representing the respective vowels /i/ and /u/,
         // ⟨i⟩ and ⟨u⟩ also typically represent the semivowels /j/ and /w/, respectively,
@@ -154,7 +179,9 @@ object ItalianRuleBased: RuleBasedTranscriber() {
             // the latin base rules already don't include j and x
             this != Regex("k") &&
             this != Regex("w") &&
-            this != Regex("y")
+            this != Regex("y") &&
+            //z is handled completely differently in italian, so the latin based rule is useless
+            this != Regex("z")
         } }
     override fun transcribe(nativeText: String): String {
         return nativeText.toLowerCase().processWithRules(rules, reportAndSkip)

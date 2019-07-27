2d56451ae1bec05cc60389c1f2d479e76a5b7e80
Adam Howard <achdisposable1+github@gmail.com>
2019-07-27 18:20:15 +0100

    feat: begin work on GUI
    Former-commit-id: 597469dc19bff7fd4372aa2a6cf0cfd528ecedb3

---
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/Gui.java
@@ -0,0 +1,39 @@
+package com.github.medavox.ipa_transcribers;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Gui extends Application {
+    //input = multi-line text input field
+    //language choice = dropdown list
+    //result = multi-line text output field
+    //errors = multi-line text output field
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

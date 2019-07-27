d227ad94d4b25b78d1ad04168b3ff97aaab95ac3
Adam Howard <achdisposable1+github@gmail.com>
2019-07-28 00:28:14 +0100

    working on GUI
    Former-commit-id: e0ad86832cb7339503ab8ce8f38ce2059a543469

--- src/main/kotlin/com/github/medavox/ipa_transcribers/Gui.java
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/Gui.java
@@ -3,10 +3,16 @@ package com.github.medavox.ipa_transcribers;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.io.PrintStream;
 
 public class Gui extends Application {
     //input = multi-line text input field
@@ -15,25 +21,61 @@ public class Gui extends Application {
     //errors = multi-line text output field
     @Override
     public void start(Stage primaryStage) {
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setHgap(10); root.setVgap(10);
+        root.setPadding(new Insets(10, 10, 10, 10));
+
+        TextArea input = new TextArea();
+        input.setEditable(true);
+        input.setPromptText("enter native text");
+        input.setWrapText(true);
+
+        TextArea output = new TextArea("transliteration goes here");
+        output.setEditable(false);
+        output.setWrapText(true);
+
+        TextArea errors = new TextArea("errors go here");
+        errors.setEditable(false);
+        errors.setWrapText(true);
+
+        String[] languages = new String[]{"Arabic", "Bengali", "English", "German", "Hindi", "Italian",
+                "Japanese/Katakana&HiraganaOnly", "Korean", "Marathi", "Malay", "Persian", "Portuguese", "Russian",
+                "Spanish/European", "Spanish/PanAmerican", "Swahili", "Telugu", "Turkish" };
+        for(String lang : languages) {
+
+        }
+        //final Image image = new Image(getClass().getResource("cross_red.png").toExternalForm(), 20, 20, true, true);
+        SplitMenuButton menuButton = new SplitMenuButton();
+        //menuButton.setGraphic(new ImageView(image));
+        menuButton.getItems().addAll(
+                new MenuItem("Really"), new MenuItem("Do not")
+        );
+
+        PrintStreamCapturer out = new PrintStreamCapturer(output, System.out);
+        PrintStreamCapturer err = new PrintStreamCapturer(errors, System.err);
+
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("Transliterate");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                //System.setErr(new PrintStream(output.));
+
             }
         });
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
 
-        Scene scene = new Scene(root, 300, 250);
+        root.add(input, 0, 0, 2, 1);
+        root.add(menuButton, 0, 1);
+        root.add(btn, 1, 1);
+        root.add(output, 0, 3, 2, 1);
+        root.add(errors, 0, 4, 2, 1);
+
+        Scene scene = new Scene(root, 400, 400);
 
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-    public static void main(String[] args) {
-        launch(args);
     }
 }
---
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/Launcher.java
@@ -0,0 +1,9 @@
+package com.github.medavox.ipa_transcribers;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Gui.class, "");
+    }
+}

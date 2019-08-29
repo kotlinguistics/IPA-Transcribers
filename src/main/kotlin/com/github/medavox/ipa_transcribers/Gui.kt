2df9ff5aa417c9d519c4c2d5d5248e57f94316ea
Adam Howard <achdisposable1+github@gmail.com>
2019-08-29 22:40:27 +0100

    refactor(Gui): remove unnecessary semicolons
    Former-commit-id: c2bc840d71df6f88c88e9e1f21fd0d47266fae48

--- src/main/kotlin/com/github/medavox/ipa_transcribers/Gui.kt
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/Gui.kt
@@ -17,24 +17,24 @@ class Gui : Application() {
     //errors = multi-line text output field
     override fun start(primaryStage:Stage) {
         val root = GridPane()
-        root.setAlignment(Pos.CENTER);
+        root.setAlignment(Pos.CENTER)
         root.setHgap(10.0); root.setVgap(10.0)
         root.setPadding(Insets(10.0, 10.0, 10.0, 10.0))
 
         val input = TextArea()
-        input.setEditable(true);
-        input.setPromptText("enter native text");
-        input.setWrapText(true);
+        input.setEditable(true)
+        input.setPromptText("enter native text")
+        input.setWrapText(true)
 
         val output = TextArea()
         output.promptText = "transliteration goes here"
-        output.setEditable(false);
-        output.setWrapText(true);
+        output.setEditable(false)
+        output.setWrapText(true)
 
         val errors = TextArea()
         errors.promptText = "errors go here"
-        errors.setEditable(false);
-        errors.setWrapText(true);
+        errors.setEditable(false)
+        errors.setWrapText(true)
         errors.text
 
         val menuButton = MenuButton()
@@ -57,7 +57,7 @@ class Gui : Application() {
         System.setErr(err)
 
         val btn = Button()
-        btn.setText("Transliterate");
+        btn.setText("Transliterate")
         btn.alignment = Pos.CENTER_RIGHT
         btn.setOnAction { event:ActionEvent ->
             System.out.println("transcribing with $selection")
@@ -67,11 +67,11 @@ class Gui : Application() {
         }
 
 
-        root.add(input, 0, 0, 2, 1);
-        root.add(menuButton, 0, 1);
-        root.add(btn, 1, 1);
-        root.add(output, 0, 3, 2, 1);
-        root.add(errors, 0, 4, 2, 1);
+        root.add(input, 0, 0, 2, 1)
+        root.add(menuButton, 0, 1)
+        root.add(btn, 1, 1)
+        root.add(output, 0, 3, 2, 1)
+        root.add(errors, 0, 4, 2, 1)
 
         val scene = Scene(root, 400.0, 400.0)
 

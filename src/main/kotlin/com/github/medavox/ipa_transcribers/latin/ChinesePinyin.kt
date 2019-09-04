7100ae4fd0cd9578d574cb9701198cad20370146
Adam Howard <achdisposable1+github@gmail.com>
2019-09-04 18:40:07 +0100

    refactor: add Chinese romanisation systems
    these haven't been started yet, but they are planned.
    It is at least one way to include Chinese, a major world language,
    in this group

---
+++ src/main/kotlin/com/github/medavox/ipa_transcribers/latin/ChinesePinyin.kt
@@ -0,0 +1,12 @@
+package com.github.medavox.ipa_transcribers.latin
+
+import com.github.medavox.ipa_transcribers.CompletionStatus
+import com.github.medavox.ipa_transcribers.RuleBasedTranscriber
+
+object ChinesePinyin: RuleBasedTranscriber() {
+    override val completionStatus: CompletionStatus = CompletionStatus.UNSTARTED
+
+    override fun transcribe(nativeText: String): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}

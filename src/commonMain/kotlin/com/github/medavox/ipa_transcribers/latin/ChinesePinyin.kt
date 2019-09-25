aca61eb2dc1fd96337789aed0fcbed40bcabcef6
Adam Howard <achdisposable1+github@gmail.com>
2019-09-25 16:07:15 +0100

    ChinesePinyin: add rules for initials

--- src/commonMain/kotlin/com/github/medavox/ipa_transcribers/latin/ChinesePinyin.kt
+++ src/commonMain/kotlin/com/github/medavox/ipa_transcribers/latin/ChinesePinyin.kt
@@ -1,13 +1,38 @@
 package com.github.medavox.ipa_transcribers.latin
 
 import com.github.medavox.ipa_transcribers.CompletionStatus
+import com.github.medavox.ipa_transcribers.Rule
 import com.github.medavox.ipa_transcribers.RuleBasedTranscriber
 
 /**Transcriber for the Hanyu Pinyin Chinese romanisation system to IPA.*/
 object ChinesePinyin: RuleBasedTranscriber() {
     override val completionStatus: CompletionStatus = CompletionStatus.UNSTARTED
 
+    private val rules:List<Rule> = listOf(
+        //initials
+        Rule("b", "p"),
+        Rule("p", "pʰ"),
+        Rule("m", "m"),
+        Rule("f", "f"),
+        Rule("d", "t"),
+        Rule("t", "tʰ"),
+        Rule("n", "n"),
+        Rule("l", "l"),
+        Rule("g", "k"),
+        Rule("k", "kʰ"),
+        Rule("h", "x"),
+        Rule("j", "tɕ"),
+        Rule("q", "tɕʰ"),
+        Rule("x", "ɕ"),
+        Rule("zh", "ʈʂ"),
+        Rule("ch", "ʈʂʰ"),
+        Rule("sh", "ʂ"),
+        Rule("r", "ɻ~ʐ"),
+        Rule("z", "ts"),
+        Rule("c", "tsʰ"),
+        Rule("s", "s")
+        )
     override fun transcribe(nativeText: String): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return nativeText.toLowerCase().processWithRules(rules, reportAndSkip)
     }
 }

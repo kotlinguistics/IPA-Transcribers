9e77f074e6030bbfc8ba39d7f3f242a09bf98f02
Adam Howard <achdisposable1+github@gmail.com>
2019-10-02 09:22:35 +0100

    ChinesePinyin: continue work
    we're going to need to use a modal transcriber
    (technically I believe it's called a finite state machine),
    which checks the current remaining input against different rules,
    depending on whether we're in INITIALS, FINAL_NO_INITIALS or FINALS mode

--- src/commonMain/kotlin/com/github/medavox/ipa_transcribers/latin/ChinesePinyin.kt
+++ src/commonMain/kotlin/com/github/medavox/ipa_transcribers/latin/ChinesePinyin.kt
@@ -6,9 +6,24 @@ import com.github.medavox.ipa_transcribers.RuleBasedTranscriber
 
 /**Transcriber for the Hanyu Pinyin Chinese romanisation system to IPA.*/
 object ChinesePinyin: RuleBasedTranscriber() {
-    override val completionStatus: CompletionStatus = CompletionStatus.UNSTARTED
+    override val completionStatus: CompletionStatus = CompletionStatus.IN_PROGRESS
 
-    private val rules:List<Rule> = listOf(
+    //Unlike European languages,
+    // clusters of letters — initials (声母; 聲母; shēngmǔ) and finals (韵母; 韻母; yùnmǔ) —
+    // and not consonant and vowel letters,
+    // form the fundamental elements in pinyin (and most other phonetic systems used to describe the Han language).
+    // Every Mandarin syllable can be spelled with exactly one initial followed by one final,
+    // except for the special syllable er or when a trailing -r is considered part of a syllable.
+    // The latter case, though a common practice in some sub-dialects, is rarely used in official publications.
+    //
+    //Even though most initials contain a consonant, finals are not always simple vowels, especially in compound finals (复韵母; 複韻母; fùyùnmǔ), i.e. when a "medial" is placed in front of the final. For example, the medials [i] and [u] are pronounced with such tight openings at the beginning of a final that some native Chinese speakers (especially when singing) pronounce yī (衣, clothes, officially pronounced /í/) as /jí/ and wéi (围; 圍, to enclose, officially pronounced /uěi/) as /wěi/ or /wuěi/. Often these medials are treated as separate from the finals rather than as part of them; this convention is followed in the chart of finals below.
+    private val mode:Mode = Mode.INITIALS
+    private enum class Mode {
+        INITIALS,
+        FINALS
+    }
+
+    private val initialRules:List<Rule> = listOf(
         //initials
         Rule("b", "p"),
         Rule("p", "pʰ"),
@@ -31,8 +46,16 @@ object ChinesePinyin: RuleBasedTranscriber() {
         Rule("z", "ts"),
         Rule("c", "tsʰ"),
         Rule("s", "s")
-        )
+    )
     override fun transcribe(nativeText: String): String {
-        return nativeText.toLowerCase().processWithRules(rules, reportAndSkip)
+        //if we don't match an initial, it's probably because there isn't one.
+        //so if no initial matches in INITIALS mode,
+        //try to match a final instead before falling back to a proper onNoRuleMatched case
+        if(mode == Mode.INITIALS) {
+
+        }else {//finals mode
+
+        }
+        return nativeText.toLowerCase().processWithRules(initialRules, reportAndSkip)
     }
 }

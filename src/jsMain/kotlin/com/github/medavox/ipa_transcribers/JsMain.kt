0e59c87a908aa9b84a78cba90abdb3d7c922396a
Adam Howard <achdisposable1+github@gmail.com>
2019-10-13 21:17:18 +0100

    feat: first working version of JS GUI
    still need to hook up the error output,
    style the page a little better,
    perform Dead Code Elimination to shrink the JS file
        (was 8.8MB upon last check!),
    and minify the JS file as an integrated step.

---
+++ src/jsMain/kotlin/com/github/medavox/ipa_transcribers/JsMain.kt
@@ -0,0 +1,37 @@
+package com.github.medavox.ipa_transcribers
+
+import com.github.medavox.UiStrings
+import org.w3c.dom.HTMLButtonElement
+import org.w3c.dom.HTMLSelectElement
+import org.w3c.dom.HTMLTextAreaElement
+import org.w3c.dom.Option
+import org.w3c.dom.events.Event
+import kotlin.browser.document
+
+fun main() {
+    val slekt = document.getElementById("lang_select") as HTMLSelectElement
+    Language.values().forEach {
+        slekt.add(Option(
+            text= it.neim,
+            value = it.name,
+            defaultSelected = false,
+            selected = false
+        ))
+    }
+
+    val inputTextArea = document.getElementById("input_text") as HTMLTextAreaElement
+    val outputTextArea = document.getElementById("output_text") as HTMLTextAreaElement
+    val errorsTextArea = document.getElementById("errors_text") as HTMLTextAreaElement
+
+    inputTextArea.setAttribute("placeholder", UiStrings.inputHint)
+    outputTextArea.setAttribute("placeholder", UiStrings.outputHint)
+    errorsTextArea.setAttribute("placeholder", UiStrings.errorsHint)
+
+
+    val button = document.getElementById("transliterate_button") as HTMLButtonElement
+    button.addEventListener("click", { event:Event ->
+        val transcribr = Language.values().firstOrNull { it.ordinal == slekt.selectedIndex }!!.transcriber
+        outputTextArea.textContent = transcribr.transcribe(inputTextArea.value)
+        errorsTextArea.textContent = "transcriber: $transcribr"
+    })
+}
